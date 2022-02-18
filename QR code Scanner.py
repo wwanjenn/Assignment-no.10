8f2e76ccdf9b9db69efda4c5385ff216c5f7a1d6
Lemonade-up <wmaglangit1118@gmail.com>
2022-02-18 19:53:55 +0800

    Pseudo Code added, First Commit

---
+++ QR code Scanner.py
@@ -0,0 +1,24 @@
+# Assignment 10
+
+# Contact Tracing App
+# 	- Create a python program that will read QRCode using your webcam
+# 	- You may use any online QRCode generator to create QRCode
+# 	- All personal data are in QRCode 
+# 	- You may decide which personal data to include
+# 	- All data read from QRCode should be stored in a text file including the date and time it was read
+
+# Note: 
+# 	- Search how to generate QRCode
+# 	- Search how to read QRCode using webcam
+# 	- Search how to create and write to text file
+# 	- Your source code should be in github before Feb 19
+# 	- Create a demo of your program (1-2 min) and send it directly to my messenger.
+
+# Steps 
+# 1 Scan QR code.
+
+# 2 Decode QR code.
+
+# 3 Check for Date and Time.
+
+# 4 Write data into a text file.
