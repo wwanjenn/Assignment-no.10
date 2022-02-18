a0b90baf93df8a32d2cf2afc032dacf1dfa33dc5
Lemonade-up <wmaglangit1118@gmail.com>
2022-02-18 20:21:14 +0800

    QR code Generator finished

--- QR code Generator.py
+++ QR code Generator.py
@@ -3,5 +3,9 @@ import qrcode
 
 with open('QR Code data.txt') as data:
     personalData = data.read()
-
+    qr = qrcode.QRCode()
+    qr.add_data(personalData)
+    qr.make(fit = True)
+    QRCODE = qr.make_image(fill = 'black', back_color ='white')
+    QRCODE.save('QR Code.png')
 
